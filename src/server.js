c5aa8e1fc76ff6fc3d1f8743cd8b62afb797d34e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-05-24 02:29:33 +0530

    feat: api done

---
+++ src/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Endpoint to retrieve the data
+app.get("/data", (req, res) => {
+  const data = {
+    totalTransaction: 10000,
+    totalLikes: 232323,
+    totalRevenues: 23232,
+    totalUsers: 323,
+    lineChartData: [
+      ["Weeks", "Guest", "User"],
+      ["Week 1", 100, 120],
+      ["Week 2", 24, 140],
+      ["Week 3", 320, 280],
+      ["Week 4", 505, 230],
+    ],
+    pieChartData: [
+      ["Task", "Hours per Day"],
+      ["Work", 55],
+      ["Eat", 32],
+      ["Commute", 12],
+    ],
+    todaysSchedule: [
+      {
+        title: "Meeting with suppliers from Kuta Mall",
+        time: "17:50 - 18:50",
+        location: "some cool location",
+      },
+      {
+        title: "Meeting with suppliers from Balli Mall",
+        time: "17:50 - 18:50",
+        location: "some cool location",
+      },
+    ],
+  };
+
+  res.json(data);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
